8baf4b0ea71e193cd7f54ce9a5a01fba4394fce3
Amanda Grace <awall8@gatech.edu>
2015-04-23 18:24:43 -0400

    fixed spin

---
+++ fingerjam/Assets/spin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+
+	bool spinning;
+	float spinDuration;
+	float spinLength;
+	float spinDecrement;
+
+	// Use this for initialization
+	void Start () {
+		spinning = true;
+		spinDecrement = Random.Range (8, 15);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spinning && spinDecrement > 0) {
+			transform.Rotate (Vector3.forward * -(spinDecrement));
+			spinDecrement -= .05f;
+		}
+	
+	}
+}
